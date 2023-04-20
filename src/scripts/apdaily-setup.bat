0b6c4d8941f9ec56304e9e761627c4d655a981d3
SarangaR <sarnga.raj@gmail.com>
2023-04-20 14:13:39 -0400

    1.0.0

--- src/scripts/apdaily-setup.bat
+++ src/scripts/apdaily-setup.bat
@@ -1,10 +1,13 @@
+@echo off
 echo "Installing apdaily"
-if "C:\Program Files\apdaily" (
-    echo "Removing old version"
-    del "C:\Program Files\apdaily"
-)
+echo:
+if exist "C:\Program Files\apdaily\" echo "Removing old version" & del "C:\Program Files\apdaily\apdaily-config.toml" /Q
+echo:
 echo "Copying files"
+echo:
 mkdir "C:\Program Files\apdaily"
-xcopy "config.toml" "C:\Program Files\apdaily\apdaily-config.toml"
-echo "Copied 1 file to C:\Program Files\apdaily"
+echo:
+set curr=%cd%
+echo F | xcopy "%curr%\scripts\config.toml" "C:\Program Files\apdaily\apdaily-config.toml"
+echo:
 echo "Install Complete"
