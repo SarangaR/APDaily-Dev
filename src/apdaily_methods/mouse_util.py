41b973cd7a5f078b12f44fd17a3eca59d90f801c
SarangaR <redshift@redshift>
2023-04-20 20:43:14 -0400

    1.0.5

--- src/apdaily_methods/mouse_util.py
+++ src/apdaily_methods/mouse_util.py
@@ -5,7 +5,7 @@ def run():
     mouse = Controller()
 
     logger = logging.getLogger("apdaily_util")
-    logging.basicConfig(level=logging.INFO, format='[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s]', datefmt='%Y-%m-%d %H:%M:%S')
+    logging.basicConfig(level=logging.INFO, format='[%(asctime)s] [%(name)s] [%(levelname)s] %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
 
     while True:
         logger.info(mouse.position)
